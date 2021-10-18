96cf01e7ae33da484207ce64df1b40bc239d8614
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-18 16:03:54 +0200

    Demo from Class - Simple movement script

---
+++ projects/SmallTheftAuto/Assets/FK/Scripts/PlayerMovementFK.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementFK : MonoBehaviour
+{
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] float rotationSpeed = 100.0f;
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
+        transform.Translate(0, translation, 0);
+        transform.Rotate(0, 0, -rotation);
+    }
+}
