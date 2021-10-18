44e2cc26f4de3c8ecb7f1b755b32bfb8fdabb0ed
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-18 18:28:43 +0200

    Cleaning up code

--- projects/SmallTheftAuto/Assets/FK/Scripts/PlayerMovementFK.cs
+++ projects/SmallTheftAuto/Assets/FK/Scripts/PlayerMovementFK.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class PlayerMovementFK : MonoBehaviour
 {
-    [SerializeField] float speed = 10.0f;
-    [SerializeField] float rotationSpeed = 100.0f;
+    [SerializeField][Range(1f,30f)] float speed = 10.0f;
+    [SerializeField][Range(60f,250f)] float rotationSpeed = 100.0f;
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-        
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(0, translation, 0);
+    }
+
+    void Rotate()
+    {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -rotation);
     }
 }
