4862babaaa7035c6d59643ae8c1938fcd8d85383
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-19 13:58:05 +0200

    🐕

--- projects/SmallTheftAuto/Assets/BT/Scripts/DogCompanionSpawnerBT.cs
+++ projects/SmallTheftAuto/Assets/BT/Scripts/DogCompanionSpawnerBT.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,14 @@ using UnityEngine.Serialization;
 public class DogCompanionSpawnerBT : MonoBehaviour {
     [FormerlySerializedAs("DogPrefab")]
     public GameObject dogPrefab;
+    private GameObject dogInstance;
     
     // Start is called before the first frame update
-    void Start() {
-        Instantiate(dogPrefab);
+    void OnEnable() {
+        dogInstance = Instantiate(dogPrefab);
+    }
+    private void OnDisable() {
+        Destroy(dogInstance);
     }
 
     // Update is called once per frame
