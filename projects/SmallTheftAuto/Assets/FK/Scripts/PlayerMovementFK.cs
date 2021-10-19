2ad85ae908f9163913705a74cca7ca91091b01d0
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-20 00:22:34 +0200

    Added Ridgidbody to player, worked on playermovement

--- projects/SmallTheftAuto/Assets/FK/Scripts/PlayerMovementFK.cs
+++ projects/SmallTheftAuto/Assets/FK/Scripts/PlayerMovementFK.cs
@@ -2,17 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerMovementFK : MonoBehaviour
 {
-    [SerializeField][Range(1f,30f)] float moveSpeed;
+    [SerializeField] float moveSpeed;
     [SerializeField][Range(60f,250f)] float rotationSpeed;
 
+    [SerializeField] [Range(0f, 1f)] [Tooltip("0 = 0% and 1 = 100% of moveSpeed")] private float precentOfMoveSpeed;
+    private Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     void Update() {
-        transform.Translate(0f, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0f); //TODO /2 when going backwards
+        
+        //transform.Translate(0f, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0f); //TODO /2 when going backwards
         transform.Translate( moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0f, 0f);
+        Move();
         // transform.Rotate(0f, 0f, -180f * Time.deltaTime * Input.GetAxis("Rotate"));
         // transform.Rotate(0f, 0f, -180f*Time.deltaTime*Input.GetAxis("Rotate2"));
 	    
@@ -20,5 +31,22 @@ public class PlayerMovementFK : MonoBehaviour
         Vector3 dir = Input.mousePosition - pos;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle -90f, Vector3.forward);
+    }
+
+    void Move()
+    {
+        
+        if (Input.GetAxis("Horizontal") >= 0)
+        {
+            rigidbody.velocity = new Vector2(0.0f, moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigidbody.velocity = new Vector2(0.0f, moveSpeed * precentOfMoveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+        }
+        
+        //input from user
+        //
     }
 }
