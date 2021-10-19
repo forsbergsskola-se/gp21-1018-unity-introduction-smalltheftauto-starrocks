a87ac800755053518b96926777e1b712f7327c62
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-19 13:35:28 +0200

    Renames

--- projects/SmallTheftAuto/Assets/BT/Scripts/CameraMotionBT.cs
+++ projects/SmallTheftAuto/Assets/BT/Scripts/CameraMotionBT.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMotion : MonoBehaviour {
+public class CameraMotionBT : MonoBehaviour {
     private Quaternion myRotation;
     // Start is called before the first frame update
     void Start()
