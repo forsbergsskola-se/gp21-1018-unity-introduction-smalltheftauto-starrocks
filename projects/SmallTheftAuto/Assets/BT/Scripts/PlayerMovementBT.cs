61590e7cabb98c4d61b99c32d0cd3f71ccfedf34
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-19 16:00:04 +0200

    Update car movement/enter & entry

--- projects/SmallTheftAuto/Assets/BT/Scripts/PlayerMovementBT.cs
+++ projects/SmallTheftAuto/Assets/BT/Scripts/PlayerMovementBT.cs
@@ -15,8 +15,11 @@ public class PlayerMovementBT : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+	    if (Input.GetKey(KeyCode.LeftShift)) {
+		    transform.Translate(0f, 7.5f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
+	    }
 	    transform.Translate(0f, 5f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
-	    transform.Translate(5f * Time.deltaTime * Input.GetAxis("Horizontal"), 0f, 0f);
+	    transform.Translate(2.5f * Time.deltaTime * Input.GetAxis("Horizontal"), 0f, 0f);
 	    // transform.Rotate(0f, 0f, -180f * Time.deltaTime * Input.GetAxis("Rotate"));
 	    // transform.Rotate(0f, 0f, -180f*Time.deltaTime*Input.GetAxis("Rotate2"));
 	    
