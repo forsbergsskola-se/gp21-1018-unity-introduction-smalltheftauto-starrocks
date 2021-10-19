32c1dbf0617672c3726a1c817408433977207578
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-19 16:13:56 +0200

    CarActiveSp refactor

--- projects/SmallTheftAuto/Assets/SP/Scripts/CarActivateSP.cs
+++ projects/SmallTheftAuto/Assets/SP/Scripts/CarActivateSP.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,16 @@ public class CarActivateSP : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private int distanceToActivate;
-    
+    private PlayerMovementSP _playerMovement;
+
+    private void Awake()
+    {
+        _playerMovement = gameObject.GetComponent<PlayerMovementSP>();
+    }
+
     private void Update()
     {
         float distance = Vector3.Distance(transform.position, player.transform.position);
-
 
         if (Input.GetKeyDown(KeyCode.E) && distance <= distanceToActivate) InteractPlayer(player.active);
         
@@ -21,18 +27,5 @@ public class CarActivateSP : MonoBehaviour
     {
         _playerMovement.enabled = isPlayerActive;
         player.SetActive(!isPlayerActive);
-
-        if (Input.GetKey(KeyCode.E) && distance <= distanceToActivate)
-        {
-            player.gameObject.SetActive(false);
-            this.gameObject.GetComponent<PlayerMovementSP>().enabled = true;
-        }
-
-        if (Input.GetKey(KeyCode.E) && !player.gameObject.active)
-        {
-            player.gameObject.SetActive(true);
-            this.gameObject.GetComponent<PlayerMovementSP>().enabled = false;
-        }
-
     }
 }
