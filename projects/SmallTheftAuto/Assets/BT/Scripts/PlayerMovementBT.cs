8a84516a4e7510798c4d936d9c074dbe8a30adb3
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-19 13:21:00 +0200

    Mouse pointer tracking

--- projects/SmallTheftAuto/Assets/BT/Scripts/PlayerMovementBT.cs
+++ projects/SmallTheftAuto/Assets/BT/Scripts/PlayerMovementBT.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerMovementBT : MonoBehaviour
 {
@@ -11,19 +14,15 @@ public class PlayerMovementBT : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(0f, 5f*Time.deltaTime*Input.GetAxis("Vertical"), 0f);
-        transform.Translate(5f*Time.deltaTime*Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.Rotate( 0f, 0f, -180f*Time.deltaTime*Input.GetAxis("Rotate"));
-        // transform.Rotate(0f, 0f, -180f*Time.deltaTime*Input.GetAxis("Rotate2"));
-        
-        RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
- 
-		if(Physics.Raycast(ray,out hit,100))
-		{
-			transform.LookAt(new Vector3(hit.point.x,transform.position.y,hit.point.z));
-		}
+    void Update() {
+	    transform.Translate(0f, 5f * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
+	    transform.Translate(5f * Time.deltaTime * Input.GetAxis("Horizontal"), 0f, 0f);
+	    transform.Rotate(0f, 0f, -180f * Time.deltaTime * Input.GetAxis("Rotate"));
+	    // transform.Rotate(0f, 0f, -180f*Time.deltaTime*Input.GetAxis("Rotate2"));
+	    
+	    Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+	    Vector3 dir = Input.mousePosition - pos;
+	    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+	    transform.rotation = Quaternion.AngleAxis(angle -90f, Vector3.forward);
     }
 }
