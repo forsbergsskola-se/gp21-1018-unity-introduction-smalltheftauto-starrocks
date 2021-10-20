3dd7acad0f96c73d31e37b5a3d10470833c777bd
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-20 09:55:42 +0200

    Worked on car script

--- projects/SmallTheftAuto/Assets/FK/Scripts/CarFK.cs
+++ projects/SmallTheftAuto/Assets/FK/Scripts/CarFK.cs
@@ -8,26 +8,45 @@ public class CarFK : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField][Range(0.1f, 100f)] private float minDistanceToEnterCar = 5f;
     CarMovementFK carMovementFk;
+    private bool playerIsInCar;
     
     void Start()
     {
         carMovementFk = GetComponent<CarMovementFK>();
         carMovementFk.enabled = false;
+        
     }
     
     void Update()
     {
-        float distance = DistanceToNearestCar();
-        if (Input.GetKeyDown(KeyCode.E)&&distance <= minDistanceToEnterCar) EnterExitCar(player.active);
+        if (playerIsInCar) {
+            ExitCar();
+        }else {
+            EnterCar();
+        }
+    }
+    void EnterCar()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && DistanceToNearestCar() <= minDistanceToEnterCar)
+        {
+            carMovementFk.enabled = true;
+            player.SetActive(false);
+            player.transform.position = transform.position;
+            playerIsInCar = true;
+        }
     }
 
-    void EnterExitCar(bool isPlayerActive)
-    { 
-        carMovementFk.enabled = isPlayerActive;
-        player.SetActive(!isPlayerActive);
-        player.transform.position = transform.position + new Vector3(-2,0, 0);
+    void ExitCar()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && DistanceToNearestCar() <= minDistanceToEnterCar)
+        {
+            carMovementFk.enabled = false;
+            player.SetActive(true);
+            player.transform.position = transform.position + new Vector3(-2,0,0);
+            playerIsInCar = false;
+        }
     }
-
+    
     float DistanceToNearestCar()
     {
         float distance = Vector3.Distance(transform.position, player.transform.position);
