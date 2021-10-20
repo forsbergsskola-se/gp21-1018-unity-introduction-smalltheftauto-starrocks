ac499d6402394f0f80e776431fff2cbd8a3199f6
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-20 15:42:24 +0200

    ZooSP - GenericClass Zoo

---
+++ projects/ZooSP/Classes/Zoo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace ZooSP.Classes
+{
+    public class Zoo<T>
+    {
+        private List<T> zooPopulation = new List<T>();
+
+        public void AddAnimal(T animal)
+        {
+            zooPopulation.Add(animal);
+        }
+    }
+}
