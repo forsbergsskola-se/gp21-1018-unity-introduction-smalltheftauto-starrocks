c5b655bdca014dcc31462ba4330283deb9cd496f
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-20 16:06:41 +0200

    Started on ZooFK exercise

--- projects/ZooFK/Program.cs
+++ projects/ZooFK/Program.cs
@@ -4,9 +4,68 @@ namespace ZooFK
 {
     class Program
     {
-        static void Main(string[] args)
+        private Zoo<Mammal> mammalZoo = new Zoo<Mammal>();
+        private Zoo<Fish> fishZoo = new Zoo<Fish>();
+        private Zoo<Donkey> donkeyZoo = new Zoo<Donkey>();
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            fishZoo.AddAnimal(new Fish());
+        }
+    }
+
+    class Animal
+    {
+        
+    }
+
+    class Mammal : Animal
+    {
+        
+    }
+    class Bear : Mammal
+    {
+        
+    }
+    class Donkey : Mammal
+    {
+        
+    }
+    class Lion : Mammal
+    {
+        
+    }
+    
+
+    class Fish : Animal
+    {
+        
+    }
+    class Salmon : Fish
+    {
+        
+    }
+    class Clownfish : Fish
+    {
+        
+    }
+
+    class Student
+    {
+        
+    }
+
+    class Zoo<TAnimal> where TAnimal : Animal 
+    {
+        public void AddAnimal(TAnimal newAnimal)
+        {
+            TAnimal animal = newAnimal;
+        }
+
+        public bool HasAnimal(TAnimal animal)
+        {
+            
         }
     }
 }
+    
+
