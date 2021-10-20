b570d3ca0b5a2169e08090e693b5dbf232ef0f12
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-20 14:30:52 +0200

    Camera

--- projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/CameraMotionBT.cs
+++ projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/CameraMotionBT.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMotionBT : MonoBehaviour {
-    private Quaternion myRotation;
     public CarMovementBT carMovement;
-    public CameraFollowVehicleBT cameraFollowVehicleBT;
-    public CameraFollowPlayerBT cameraFollowPlayerBT;
-    // Start is called before the first frame update
-    void Start()
-    {
-        myRotation = this.transform.rotation;
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (carMovement.enabled) {
-            cameraFollowPlayerBT.enabled = false;
-            cameraFollowVehicleBT.enabled = true;
+            GetComponent<CameraFollowPlayerBT>().enabled = false;
+            GetComponent<CameraFollowVehicleBT>().enabled = true;
         }
         else {
-            cameraFollowVehicleBT.enabled = false;
-            cameraFollowPlayerBT.enabled = true;
+            GetComponent<CameraFollowVehicleBT>().enabled = false;
+            GetComponent<CameraFollowPlayerBT>().enabled = true;
         }
-        this.transform.rotation = myRotation;
 
     }
 }
---
+++ projects/ZooBT/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ZooBT
+{
+    class Animal {
+        
+    }
+    class Mammal : Animal {
+        
+    }
+    class Bear : Mammal {
+        
+    }
+    class Donkey : Mammal {
+        
+    }
+    class Lion : Mammal {
+        
+    }
+    class Fish : Animal {
+        
+    }
+    class Salmon : Fish {
+        
+    }
+    class Clownfish : Fish {
+        
+    }
+    class Student {
+        
+    }
+    class FishZoo <creature> where creature : Fish {
+        public void Activate(creature Salmon) {
+           
+        }
+    }
+    class MamalZoo <creature> where creature : Mammal  {
+        public void Activate(creature Lion) {
+            
+        }
+    }
+    class DonkeyZoo <creature> where creature : Donkey{
+        public void Activate(creature Donkey) {
+            
+        }
+    }
+
+    class Program
+    {
+        public void AddAnimal() {
+                
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
