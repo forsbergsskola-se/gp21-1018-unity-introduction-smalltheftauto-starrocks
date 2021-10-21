aa2579010c42861e27ee6fd3a32be493134acbad
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-21 16:55:41 +0200

    PlayerMovement fixed

--- projects/SmallTheftAuto/Assets/Scripts/Player/PlayerMovement.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]float moveSpeed =10f;
-    [SerializeField]float rotateSpeed = 180f;
+    [SerializeField]float rotateSpeed;
     void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
     {
         transform.Translate(0f, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0f);
         transform.Rotate(0f,0f,rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
