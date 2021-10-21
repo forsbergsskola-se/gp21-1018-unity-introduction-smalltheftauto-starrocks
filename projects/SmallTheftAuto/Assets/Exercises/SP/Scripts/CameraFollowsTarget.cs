48dc317b76314abbbc17ffbed16efbd84915a163
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-21 11:57:10 +0200

    DemorFromClass - CameraFollowsPLayer

---
+++ projects/SmallTheftAuto/Assets/Exercises/SP/Scripts/CameraFollowsTarget.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowsTarget : MonoBehaviour
+{
+    public Transform player;
+    
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.01f);
+    }
+}
