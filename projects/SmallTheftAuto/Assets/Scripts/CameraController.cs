494ec30f2ab914925475df18bb491797ca946910
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-21 17:33:39 +0200

    Camera follows Player

--- projects/SmallTheftAuto/Assets/Scripts/CameraController.cs
+++ projects/SmallTheftAuto/Assets/Scripts/CameraController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CameraController : MonoBehaviour {
+    Vector3 offset;
+    [SerializeField]Transform player;
+    Vector3 velocity = Vector3.zero;
+    [SerializeField]float smoothTime = 0.3f;
+    void Start() {
+        offset = transform.position - player.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
-    }
+	    CameraFollowsPlayer(); //This needs to change when VEH is added
+	}
+
+	void CameraFollowsPlayer() {
+	    Vector3 targetPosition = player.position + offset;
+	    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
 }
