08a72ea546cb64cd781bdf8623ac37bb1fff407a
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-21 10:57:08 +0200

    Camera Updates

--- projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/CameraMotionBT.cs
+++ projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/CameraMotionBT.cs
@@ -2,17 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CameraMotionBT : MonoBehaviour {
     public CarMovementBT carMovement;
-    public Transform Player;
-    public Transform Vehicle;
+    public Transform player;
+    public Transform vehicle;
     public Transform camTransform;
-    public Vector3 Offset;
+    public Vector3 offset;
     public float SmoothTime = 0.3f;
     private Vector3 velocity = Vector3.zero;
     private void Start() {
-        Offset = camTransform.position - Player.position;
+        offset = camTransform.position - player.position;
     }
     private void LateUpdate() {
         if (carMovement.enabled) {
@@ -23,13 +24,13 @@ public class CameraMotionBT : MonoBehaviour {
         }
     }
     void CameraFollowPlayer() {
-        Vector3 targetPosition = Player.position + Offset;
+        Vector3 targetPosition = player.position + offset;
         camTransform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
-        transform.LookAt(Player);
+        transform.LookAt(player);
     }
     void CameraFollowVehicle() {
-        Vector3 targetPosition = Vehicle.position + Offset;
+        Vector3 targetPosition = vehicle.position + offset;
         camTransform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
-        transform.LookAt(Vehicle);
+        transform.LookAt(vehicle);
     }
 }
