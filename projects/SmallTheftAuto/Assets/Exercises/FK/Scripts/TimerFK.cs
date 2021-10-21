4e7544c41e45768e77eb00c94526e9a021798391
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-21 16:24:44 +0200

    character animation testing

---
+++ projects/SmallTheftAuto/Assets/Exercises/FK/Scripts/TimerFK.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerFK : MonoBehaviour
+{
+    float timePassed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     timePassed = Time.deltaTime;
+    //     (timePassed.ToString("0.00s"));
+    // }
+}
