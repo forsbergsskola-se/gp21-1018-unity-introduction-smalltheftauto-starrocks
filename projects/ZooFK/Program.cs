3d10e7506f8f016ede133dc990d10b1700e8b0ff
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-10-21 11:33:01 +0200

    HasAnimal working

--- projects/ZooFK/Program.cs
+++ projects/ZooFK/Program.cs
@@ -1,69 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ZooFK
 {
     class Program
     {
-        private Zoo<Mammal> mammalZoo = new Zoo<Mammal>();
-        private Zoo<Fish> fishZoo = new Zoo<Fish>();
-        private Zoo<Donkey> donkeyZoo = new Zoo<Donkey>();
+        public class Animal{}
+        public class Mammal : Animal{}
+        public class Bear : Mammal{}
+        public class Donkey : Mammal{}
+        public class Lion : Mammal{}
+        public class Fish : Animal{}
+        public class Salmon : Fish{}
+        public class Clownfish : Fish{}
+        public class Student{}
+        
+ 
+        
         static void Main()
         {
-            fishZoo.AddAnimal(new Fish());
+        Zoo<Mammal> mammalZoo = new Zoo<Mammal>();
+        Zoo<Fish> fishZoo = new Zoo<Fish>();
+        Zoo<Donkey> donkeyZoo = new Zoo<Donkey>();
+        fishZoo.AddAnimal(new Fish());
+        
+        Console.WriteLine(fishZoo.HasAnimal<Fish>());
+        Console.WriteLine(fishZoo.HasAnimal<Donkey>());
         }
-    }
-
-    class Animal
-    {
-        
-    }
-
-    class Mammal : Animal
-    {
-        
-    }
-    class Bear : Mammal
-    {
-        
-    }
-    class Donkey : Mammal
-    {
-        
-    }
-    class Lion : Mammal
-    {
-        
-    }
-    
-
-    class Fish : Animal
-    {
-        
-    }
-    class Salmon : Fish
-    {
-        
-    }
-    class Clownfish : Fish
-    {
-        
-    }
-
-    class Student
-    {
-        
-    }
-
-    class Zoo<TAnimal> where TAnimal : Animal 
-    {
-        public void AddAnimal(TAnimal newAnimal)
+        class Zoo<TAnimal> where TAnimal : Animal
         {
-            TAnimal animal = newAnimal;
-        }
+            private List<TAnimal> animals = new List<TAnimal>();
 
-        public bool HasAnimal(TAnimal animal)
-        {
-            
+            public void AddAnimal(TAnimal animal)
+            {
+                this.animals.Add(animal);
+            }
+
+            public bool HasAnimal<TSpecies>()
+            {
+                for (int i = 0; i < animals.Count(); i++)
+                {
+                    if (typeof(TSpecies) == animals[i].GetType())
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
     }
 }
