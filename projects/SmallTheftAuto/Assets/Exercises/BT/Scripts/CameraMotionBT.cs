ae06e5951f2a9e0c125faa08a515862fe1e07f1c
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-21 10:35:57 +0200

    Consolidate Camera Scripts

--- projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/CameraMotionBT.cs
+++ projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/CameraMotionBT.cs
@@ -1,20 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMotionBT : MonoBehaviour {
     public CarMovementBT carMovement;
-
-    void Update()
-    {
+    public Transform Player;
+    public Transform Vehicle;
+    public Transform camTransform;
+    public Vector3 Offset;
+    public float SmoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    private void Start() {
+        Offset = camTransform.position - Player.position;
+    }
+    private void LateUpdate() {
         if (carMovement.enabled) {
-            GetComponent<CameraFollowPlayerBT>().enabled = false;
-            GetComponent<CameraFollowVehicleBT>().enabled = true;
+            CameraFollowVehicle();
         }
         else {
-            GetComponent<CameraFollowVehicleBT>().enabled = false;
-            GetComponent<CameraFollowPlayerBT>().enabled = true;
+            CameraFollowPlayer();
         }
-
+    }
+    void CameraFollowPlayer() {
+        Vector3 targetPosition = Player.position + Offset;
+        camTransform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+        transform.LookAt(Player);
+    }
+    void CameraFollowVehicle() {
+        Vector3 targetPosition = Vehicle.position + Offset;
+        camTransform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+        transform.LookAt(Vehicle);
     }
 }
