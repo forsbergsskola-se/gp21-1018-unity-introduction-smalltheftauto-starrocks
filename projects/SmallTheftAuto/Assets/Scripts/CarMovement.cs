1dd616597a2635b8daddfeef53dc53aac45ff76d
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-22 13:29:49 +0200

    Car Movement

--- projects/SmallTheftAuto/Assets/Scripts/CarMovement.cs
+++ projects/SmallTheftAuto/Assets/Scripts/CarMovement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField][Range(1f, 20f)]float moveSpeed = 10f;
+    [SerializeField][Range(0f, 360f)]float rotateSpeed;
     void Update()
     {
-        
+        CarMove();
+    }
+
+    void CarMove()
+    {
+        transform.Translate(0f, 0f,moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"));
+        transform.Rotate(0f,rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0f);
     }
 }
