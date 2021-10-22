de53c3b0379717d2a2dcf3155295ccf8a7989b28
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-22 11:08:10 +0200

    Exercises - Healthbar changes color over scale

--- projects/SmallTheftAuto/Assets/Exercises/SP/Scripts/HUD/HealthBarScale.cs
+++ projects/SmallTheftAuto/Assets/Exercises/SP/Scripts/HUD/HealthBarScale.cs
@@ -2,20 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
+
+[RequireComponent(typeof(Image))]
 public class HealthBarScale : MonoBehaviour
 {
-    private PlayerStats _playerStats;
     [SerializeField] private Vector3 scaleOffset;
-
+    [SerializeField] [Range(0, 1)] private float yellowBar;
+    [SerializeField] [Range(0, 1)] private float redBar;
+    
+    private PlayerStats _playerStats;
+    private Image _image;
+    
     private void Awake()
     {
         _playerStats = GetComponentInParent<PlayerStatsLoader>().playerStats;
+        _image = GetComponent<Image>();
+
     }
 
     private void Update()
     {
         transform.localScale = CalcScale();
+        Tint();
     }
 
     private Vector3 CalcScale()
@@ -25,5 +36,12 @@ public class HealthBarScale : MonoBehaviour
         float scaleZ = scaleOffset.z;
 
         return new Vector3(scaleX, scaleY, scaleZ);
+    }
+
+    private void Tint()
+    {
+        if (_playerStats.Health < redBar) _image.color = Color.red;
+        else if (_playerStats.Health < yellowBar) _image.color = Color.yellow;
+        else _image.color = Color.green;
     }
 }
