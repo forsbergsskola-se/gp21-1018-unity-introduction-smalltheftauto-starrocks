5101ef26a15690744fb3521294c27f84cf03cbb3
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-10-22 15:28:01 +0200

    Damage on Coll

--- projects/SmallTheftAuto/Assets/Scripts/CarHealth.cs
+++ projects/SmallTheftAuto/Assets/Scripts/CarHealth.cs
@@ -4,15 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarHealth : MonoBehaviour {
-	private int health;
-	private int maxHealth;
-	
-	public int Health {
-		get;
+	[SerializeField]private int health;
+	[SerializeField]private int maxHealth;
+	[SerializeField]private int damage;
+
+	private void Awake() {
+		health = maxHealth;
 	}
-	
 	void TakeDamage(int damage) {
 		health -= damage;
 		health = Mathf.Clamp(health, 0 , maxHealth);
+	}
+	private void OnCollisionEnter(Collision other) {
+		if (other.gameObject.CompareTag("Vehicle")) {
+			TakeDamage(damage);
+		}
 	}
 }
