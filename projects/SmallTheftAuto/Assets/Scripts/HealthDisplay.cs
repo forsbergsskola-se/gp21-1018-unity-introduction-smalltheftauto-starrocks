070918cee0a774cef849270ab598df8962119db0
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-26 15:34:21 +0200

    Update HealthDisplay.cs

--- projects/SmallTheftAuto/Assets/Scripts/HealthDisplay.cs
+++ projects/SmallTheftAuto/Assets/Scripts/HealthDisplay.cs
@@ -1,21 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
     private PlayerStats _playerStats;
+    private Image _image;
 
     private void Awake()
     {
         _playerStats = GetComponentInParent<PlayerStatsLoader>().playerStats;
+        _image = GetComponent<Image>();
     }
 
     private void LateUpdate()
     {
-        float health  = _playerStats.Health;
-        Vector3 scaleHealthBar = new Vector3(health /50, 0.2f, 1);
+        ScaleHealthBar();
+        if(_playerStats.Health < 25) _image.color = Color.red;
+    }
+
+    private void ScaleHealthBar()
+    {
+        float health = _playerStats.Health;
+        Vector3 scaleHealthBar = new Vector3(health / 50, 0.2f, 1);
         transform.localScale = scaleHealthBar;
     }
 }
