a0bed91208c4db9cc597094d57c12005e102487f
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-26 14:42:42 +0200

    Playerstats - Scriptable Objects

---
+++ projects/SmallTheftAuto/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Player/Stats", fileName = "Stats")]
+public class PlayerStats : ScriptableObject
+{
+   [SerializeField] private int health;
+   [SerializeField] private int money;
+   [SerializeField] private int score;
+
+   public int Health => health;
+   public float Money => money;
+   public float Score => score;
+}
