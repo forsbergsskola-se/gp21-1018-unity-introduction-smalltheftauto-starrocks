fc4fc35389ae99f8a200c102c566e77e79c2c7f3
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-11-01 15:28:59 +0100

    MainMenu

--- projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/MainMenuBT.cs
+++ projects/SmallTheftAuto/Assets/Exercises/BT/Scripts/MainMenuBT.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuBT : MonoBehaviour
-{
-
+public class MainMenuBT : MonoBehaviour {
+	public GameObject restartGame;
+	public GameObject close;
+	public void MainMenu() {
+		restartGame.SetActive(true);
+		close.SetActive(true);
+	}
+	public void MainMenuClose() {
+		restartGame.SetActive(false);
+		close.SetActive(false);
+	}
 }
