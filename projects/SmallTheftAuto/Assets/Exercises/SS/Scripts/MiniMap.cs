35b7c853db504e0c111a2ce7b92887e02327e2e1
Sara SABER <sarasaberdev@gmail.com>
2021-11-02 20:31:32 +0300

    did the minimap on personal folder and scripts

---
+++ projects/SmallTheftAuto/Assets/Exercises/SS/Scripts/MiniMap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Transform miniMapCameraTransform;
+    public Transform playerTransform;
+    private Vector3 cameraFromPlayerOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraFromPlayerOffset = new Vector3(0, 14, 0);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        miniMapCameraTransform.position = playerTransform.position+ cameraFromPlayerOffset;
+    }
+}
