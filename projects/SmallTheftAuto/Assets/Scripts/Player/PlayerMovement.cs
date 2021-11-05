956e202b0a8790e821d6ca3e422936a2d08e6e52
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-11-05 14:00:34 +0100

    Playermovement work

--- projects/SmallTheftAuto/Assets/Scripts/Player/PlayerMovement.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,28 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField][Range(1f, 20f)]float moveSpeed = 10f;
     [SerializeField][Range(0f, 360f)]float rotateSpeed;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         PlayerMove();
+        AnimatePlayer();
     }
 
     void PlayerMove()
     {
-        transform.Translate(0f, 0f,moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"));
+        //transform.Translate(0f, 0f,moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"));
         transform.Rotate(0f,rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0f);
+    }
+
+    void AnimatePlayer()
+    {
+        _animator.SetFloat("walkSpeed" ,Input.GetAxis("Vertical"));
+        Debug.Log(_animator.GetFloat("walkSpeed"));
     }
 }
