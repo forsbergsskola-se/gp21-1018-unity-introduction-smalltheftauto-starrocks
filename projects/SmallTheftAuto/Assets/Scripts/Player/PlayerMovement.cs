3f94fe4d63594d36c4d1f2cae9fb3816d0fef65f
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-11-05 14:16:32 +0100

    Old movement restored(suck my balls animator)

--- projects/SmallTheftAuto/Assets/Scripts/Player/PlayerMovement.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,28 +7,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField][Range(1f, 20f)]float moveSpeed = 10f;
     [SerializeField][Range(0f, 360f)]float rotateSpeed;
-    private Animator _animator;
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-    }
-
+    
     void Update()
     {
         PlayerMove();
-        AnimatePlayer();
     }
 
     void PlayerMove()
     {
-        //transform.Translate(0f, 0f,moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"));
+        transform.Translate(0f, 0f,moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"));
         transform.Rotate(0f,rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0f);
-    }
-
-    void AnimatePlayer()
-    {
-        _animator.SetFloat("walkSpeed" ,Input.GetAxis("Vertical"));
-        Debug.Log(_animator.GetFloat("walkSpeed"));
     }
 }
