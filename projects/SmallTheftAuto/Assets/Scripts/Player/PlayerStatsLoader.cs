19475c2c09aabb82b33beceb7291f70715e28049
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-11-05 12:14:47 +0100

    Update save

--- projects/SmallTheftAuto/Assets/Scripts/Player/PlayerStatsLoader.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Player/PlayerStatsLoader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,9 @@ using UnityEngine;
 public class PlayerStatsLoader : MonoBehaviour
 {
     public PlayerStats playerStats;
+
+    private void Awake()
+    {
+        SaveSystem.LoadPlayer();
+    }
 }
