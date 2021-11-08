38b73a7de8631c5b372f1cdf468cb461a6451a77
InaSLew <ina.shenlewander@gmail.com>
2021-11-08 17:50:03 +0100

    Comments on inheritance vs composition

--- projects/SmallTheftAuto/Assets/Scripts/Traffic/PedestrianHealth.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Traffic/PedestrianHealth.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+// Note:
+// Composition-wise I would say having a class called PedestrianHealth inheriting from TrafficCarHealth is a little confusing
+// I suggest extracting the logic inside TrafficCarHealth that's shared by both TrafficCar and Pedestrian into a different base class
+// and then have TrafficCar and Pedestrian inherit from it
+
 public class PedestrianHealth : TrafficCarHealth
 {
     private void LateUpdate()
--- projects/SmallTheftAuto/Assets/Scripts/Ui/GunDisplay.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Ui/GunDisplay.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GunDisplay : MonoBehaviour
 {
+	// Note:
+	// Consider deleting the script if it's not currently being used
 	void ShowWeapon() {
 		
 	}
