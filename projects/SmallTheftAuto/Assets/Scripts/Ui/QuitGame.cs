0a7325c6ab8e0a074fdeb5adba01e9b879f99750
FellanNokes <felix.kjellberg@student.forsbergsskola.se>
2021-11-08 16:44:57 +0100

    yeah Ben :)

--- projects/SmallTheftAuto/Assets/Scripts/Ui/QuitGame.cs
+++ projects/SmallTheftAuto/Assets/Scripts/Ui/QuitGame.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class QuitGame : MonoBehaviour
 {
     //Method should be named with capital letter :)
+    //Agreed /Felix
     public void doExitGame() {
         Application.Quit();
         Debug.Log("Game Quit");
